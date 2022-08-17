86be1f020435f15b7c698a60d101eb057eaf462c
pankaspe <mez.tnt@gmail.com>
2022-08-17 10:59:29 +0200

    first deploy

--- lib/config/website.tsx
+++ lib/config/website.tsx
@@ -4,7 +4,7 @@ const websiteConfig = {
       title: "Anzasca",
       subTitle: "Un repository con dati botanici derivati dalla mappatura del territorio della Valle Anzasca.",
       keywords: "Valle Anzasca, natura, ossola, botanica, piante, escursioni, montagna",
-      ogImage: "/vercel.svg",
+      ogImage: "/landscape.png",
       logo: "/logo.svg",
       favicon: "/favicon.ico"
    },
